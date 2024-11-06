295c4039aab4582e4f1af1507979823876874349
Michael James <mikeyj777@gmail.com>
2024-11-06 18:09:16 -0500

    3

--- client/src/components/Proj003ToDoList.js
+++ client/src/components/Proj003ToDoList.js
@@ -10,15 +10,25 @@ const Proj003ToDoList = () => {
 
   const handeNewTodo = (event) => {
     setNewTodo(event.target.value);
-    event.target.value = '';
   }
 
   const handleSubmit = (event) => {
     if (event.key === 'Enter' && newTodo.trim()) {
-      event.preventDefault();
-      setTodos([...todos, newTodo]);
-      setNewTodo('');
+    event.preventDefault();
+    setTodos([...todos, newTodo]);
+    setNewTodo('');
     }
+  }
+
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  }
+
+  const removeLastTodo = () => {
+    if (!todos.length) return;
+    setTodos(todos => todos.slice(0,-1));
   }
 
   return (
@@ -33,20 +43,71 @@ const Proj003ToDoList = () => {
         onKeyDown={handleSubmit}
         label="new To Do"
       />
-      <div className="array-display">
-        <span className="array-display__bracket">[</span>
-          {todos.map((item, index) => (
-              <span key={index}>
-                  {item}
-                  {index < todos.length - 1 && (
-                      <span className="array-display__comma">,</span>
-                  )}
-              </span>
-          ))}
-          <span className="array-display__bracket">]</span>
+      <div className="todo-container">
+        {todos.map((todo, index) => (
+            <div key={index} className="todo-item">
+                <span className="todo-text">{todo}</span>
+                <CustomButton
+                    variant="minus"
+                    onClick={() => removeTodo(index)}
+                />
+            </div>
+        ))}
+      </div>
+      <div className="todo-pop">
+        <CustomButton variant="pop" onClick={() => removeLastTodo()} label="Remove Last Todo">
+          Remove Last Todo
+        </CustomButton>
+
       </div>
     </div>
   );
 };
 
 export default Proj003ToDoList;
+
+
+
+
+
+// const removeTodo = (index) => {
+//   const updatedTodos = [...todos];
+//   updatedTodos.splice(index, 1);
+//   setTodos(updatedTodos);
+// }
+
+// const removeLastTodo = () => {
+//   if (!todos.length) return;
+//   setTodos(todos => todos.slice(0,-1));
+// }
+
+// return (
+//   <div className="component-group">
+//     <h1>Project 3</h1>
+//     <h2>Project 3 ToDo List</h2>
+//     <input 
+//       className=".custom-input" 
+//       type="text" 
+//       value={newTodo} 
+//       onChange={handeNewTodo} 
+//       onKeyDown={handleSubmit}
+//       label="new To Do"
+//     />
+//     <div className="todo-container">
+//       {todos.map((todo, index) => (
+//           <div key={index} className="todo-item">
+//               <span className="todo-text">{todo}</span>
+//               <CustomButton
+//                   variant="minus"
+//                   onClick={() => removeTodo(index)}
+//               />
+//           </div>
+//       ))}
+//     </div>
+//     <CustomButton
+//         variant="minus"
+//         onClick={removeLastTodo()}
+//         label="Remove Last Todo"
+//     />
+//   </div>
+// );
